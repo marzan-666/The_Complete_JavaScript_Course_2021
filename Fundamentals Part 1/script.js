64f902005fd3955d024abea8ac24fe26518d7144
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-01-30 03:22:40 +0600

    Update script.js

--- Fundamentals Part 1/script.js
+++ Fundamentals Part 1/script.js
@@ -202,3 +202,31 @@ console.log('23' > '18');
 let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
+
+////////////////////////////////////
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is UNDEFINED');
+}
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(1));
+console.log('Marzan',Boolean('Marzan'));
