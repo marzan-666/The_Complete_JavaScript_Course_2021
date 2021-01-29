a2366fce2c8e73be120386c9cb51b7bd861c6f36
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-01-30 02:54:28 +0600

    Update script.js

--- Fundamentals Part 1/script.js
+++ Fundamentals Part 1/script.js
@@ -178,3 +178,27 @@ if(AGE >=18){
 }else{
   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
 }
+
+// Type Conversion and Coercion
+
+// type conversion
+
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('jonas'));
+console.log(Number('1994'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
