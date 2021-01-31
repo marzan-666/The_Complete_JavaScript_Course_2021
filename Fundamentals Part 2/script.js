43e66f2b63c505e3cb3f424716aaa56355c49220
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-01-31 20:31:40 +0600

    Update script.js

--- Fundamentals Part 2/script.js
+++ Fundamentals Part 2/script.js
@@ -253,3 +253,46 @@ const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length -
 console.log(ages);
 
 */
+
+///////////////////////////////////////
+// Basic Array Operations (Methods)
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+const newLength2 = friends.push('Marzan');
+console.log(friends);
+console.log(newLength);
+console.log(newLength2);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}else{
+  console.log('No Friend found');
+}
+if(friends.includes('Marzan')){
+  console.log('You have a friend called Marzan');
+}else{
+  console.log('No Friend found');
+}
