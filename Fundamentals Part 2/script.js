8b7bf737baf8be064bdad7688a6a757298bde1b6
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-04 20:29:03 +0600

    Update script.js

--- Fundamentals Part 2/script.js
+++ Fundamentals Part 2/script.js
@@ -564,4 +564,20 @@ for (let i = 0; i < years.length; i++) {
   ages.push(2037 - years[i]);
 }
 console.log(ages);
+
+// continue and break
+
+console.log('--- ONLY STRINGS ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== 'string') continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === 'number') break;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
 */
