ef680401e0abfcbc40ef42602064d2d3df1ef392
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-04 20:21:23 +0600

    Update script.js

--- Fundamentals Part 2/script.js
+++ Fundamentals Part 2/script.js
@@ -526,3 +526,42 @@ if (mark.bmi > john.bmi) {
 for (let rep = 1; rep <= 30; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 }
+
+///////////////////////////////////////
+// Looping Arrays, Breaking and Continuing
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true
+];
+const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  // types[i] = typeof jonas[i];
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+*/
