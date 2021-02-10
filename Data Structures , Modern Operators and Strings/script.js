d92bed1efc1c82e30eb070bed987678221f1dc21
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-11 05:02:48 +0600

    Add files via upload

---
+++ Data Structures , Modern Operators and Strings/script.js
@@ -0,0 +1,134 @@
+'use strict';
+/*
+///// Destructuring Arrays from line 
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: { // Destructuring Objects 
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0,time = '20:00', address}){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+
+  }
+
+};
+
+restaurant.orderDelivery ({  // Destructuring Objects 
+  time:'22:30',
+  address: 'Puran Dhaka',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery ({
+  //time:'22:30',
+  address: 'Puran Dhaka',
+  //mainIndex: 2,
+  starterIndex: 1,
+});
+
+const {name,openingHours,categories} = restaurant;
+console.log(name,openingHours,categories);
+
+const{
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName,hours,tags);
+
+   // default values
+
+const {menu = [], starterMenu: starters = [] }= restaurant;
+console.log(menu,starters);
+
+// Mutating variables
+
+let a1 = 111;
+let b1 = 999;
+let c1 = 888;
+const obj = {a1: 23, b1: 7, c1: 14};
+
+({a1,b1,c1} = obj ) ;
+
+console.log(a1,b1,c1);
+
+// Nested objects
+
+const { fri:{open,close} } = openingHours;  // Destructuring Objects 
+//console.log(fri);
+console.log(open,close);
+
+///// Destructuring Arrays
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr ;
+console.log(x,y,z);
+console.log(arr);
+
+//const [first, , second] = restaurant.categories; // putting a coma means indicatin the 3rd element
+//console.log(first,second);
+
+let [main, ,secondary] = restaurant.categories;
+console.log('Before switching:',main,secondary);
+
+
+// switching variables
+
+//const temp = main;
+//main = secondary;
+//secondary = temp;
+//console.log(main,secondary);
+
+[main,secondary] = [secondary,main]; // switching variables 2
+console.log('After switching:',main,secondary);
+
+console.log(restaurant.order(2,0));
+
+const [starter, mainCourse] = restaurant.order(2,0);
+console.log(starter,mainCourse);
+
+// Nested destructuring
+
+const nested = [2, 4, [5,6]]; // nested array
+
+//const [i, , j] = nested;
+//console.log(i,j);
+
+const [i, , [j,k]] = nested;
+console.log(i,j,k); // separated values of array 
+
+// Default values
+
+const [p,q,r] = [8,9];
+console.log(p,q,r);
+
+const[p1=1,q1=1,r1=1] = [8,9]; // if we remove 9 the q will be 1
+console.log(p1,q1,r1);
+
+*/
