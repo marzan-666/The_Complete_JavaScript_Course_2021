1fc7fa09a7c52fe698455ca206bb8d663aa28638
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-11 21:35:07 +0600

    Update script.js

--- Data Structures , Modern Operators and Strings/script.js
+++ Data Structures , Modern Operators and Strings/script.js
@@ -325,3 +325,98 @@ add(...x);
 restaurant.orderPizza('mushrooms', 'onion', 'olives' , 'spinach');
 restaurant.orderPizza('mushrooms');
 */
+
+
+/*
+////// Short circuiting (&& and ||)
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: { 
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery: function({
+    starterIndex = 1, 
+    mainIndex = 0,
+    time = '20:00', 
+    address,
+  }){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta: function(ing1,ing2,ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza : function(mainIngredient, 
+    ...otherIngredients){
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    }
+
+};
+
+
+// Use ANY data type, return ANY data type, short-circuiting
+
+console.log(3 || 'Marzan');
+console.log('' || 'Marzan');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); // Hello is the first truthy value
+
+
+//restaurant.numGuests = 23 ;
+restaurant.numGuests = 0 ;
+const guests1 = restaurant.numGuests ? restaurant.numGuests :10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+
+console.log(0 && 'Marzan'); // opposite of || (OR) operator
+console.log(7 && 'Marzan'); // only true if all elements are true
+
+console.log('Hello' && 23 && null && 'Marzan');
+
+// Practical example
+if(restaurant.orderPizza){
+  restaurant.orderPizza('mushrooms','spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms','spinach');
+
+
+///// The Nullish Coalescing Operator (??)
+
+//restaurant.numGuest = 0; // Nullis: null and undefined
+const guest = restaurant.numGuest || 10 ;
+console.log(guest);
+
+const guessCorrect = restaurant.numGuest ?? 10;
+console.log(guessCorrect);
+*/
