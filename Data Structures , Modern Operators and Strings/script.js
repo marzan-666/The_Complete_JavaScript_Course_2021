4d46680ea5e04f295a2f02d732cb39ab13b74a2d
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-12 19:58:08 +0600

    Update script.js

--- Data Structures , Modern Operators and Strings/script.js
+++ Data Structures , Modern Operators and Strings/script.js
@@ -531,3 +531,70 @@ printGoals('Lewandoski','Gnarby','Lewandoski','Hummels');
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
 */
+
+
+////// Looping arrays , The for-of Loop
+/*
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: { 
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery: function({
+    starterIndex = 1, 
+    mainIndex = 0,
+    time = '20:00', 
+    address,
+  }){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta: function(ing1,ing2,ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza : function(mainIngredient, 
+    ...otherIngredients){
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    }
+
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for(const item of menu) console.log(item);
+
+for(const item of menu.entries()){
+  //console.log(item);
+  console.log(`${item[0] +1 } : ${item[1]}`);
+  
+}
+
+for(const [i,el] of menu.entries()){
+  //console.log(item);
+  //console.log(`${item[0] +1 } : ${item[1]}`);
+  console.log(`${i+1} : ${el}`);
+}
+//console.log(...menu.entries());
+*/
