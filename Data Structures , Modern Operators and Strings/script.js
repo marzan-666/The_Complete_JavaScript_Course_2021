f3bfd1bb9c44432f76e0173c795c299013309f9f
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-12 21:57:48 +0600

    Update script.js

--- Data Structures , Modern Operators and Strings/script.js
+++ Data Structures , Modern Operators and Strings/script.js
@@ -652,3 +652,91 @@ const restaurant = {
 
 };
 */
+
+/*
+///////////  Optional Chaining
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: { 
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery: function({
+    starterIndex = 1, 
+    mainIndex = 0,
+    time = '20:00', 
+    address,
+  }){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta: function(ing1,ing2,ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza : function(mainIngredient, 
+    ...otherIngredients){
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    }
+
+};
+
+//console.log(restaurant.openingHours.mon.open);
+if(restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+console.log(restaurant.openingHours.fri.open);
+
+// with optional chaining
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+
+const days = ['mon', 'tue', 'wed', 'thu','fri','sat', 'sun'];
+
+for(const day of days){
+  console.log(day);
+  //const open = restaurant.openingHours[day]?.open;
+  //const open = restaurant.openingHours[day]?.open || 'closed'; // but for this or operator sat will show closed because it opens at 0 and 0 is falsy value
+  const open = restaurant.openingHours[day]?.open ?? 'closed'; // solution and its called Nullish Coalescing , optional chaining and nullish coalescing working together
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+
+console.log(restaurant.order?.(0,1)?? 'Method does not exist');
+console.log(restaurant.orderBiriyani?.(0,1) ?? 'Method does not exist');
+
+// Arrays
+
+const users = [
+  {name: 'Marzan', email: 'marzan@javascript.com'}
+];
+
+console.log(users[0]?.name ?? 'User Array is Empty'); // instead of using if else
+
+if(users.length > 0) console.log(users[0].name);
+else console.log('User array is Empty');
+*/
