57faa985d785824e127ed58d5e14b2fa91c53727
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-13 21:18:51 +0600

    Update script.js

--- Data Structures , Modern Operators and Strings/script.js
+++ Data Structures , Modern Operators and Strings/script.js
@@ -806,3 +806,131 @@ console.log(new Set(['Waiter','Chef','Waiter','Manager','Chef','Waiter']).size);
 
 console.log(new Set('Marzan Khan').size);
 */
+
+
+///// Maps : Fundamentals
+/*
+const day =['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours = { 
+  [day[3]]: { // instead of thu:
+    open: 12,
+    close: 22,
+  },
+  [day[4]]: { // instead of fri:
+    open: 11,
+    close: 23,
+  },
+  [day[5]]: {  // instead of sat:
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery({
+    starterIndex = 1, 
+    mainIndex = 0,
+    time = '20:00', 
+    address,
+  }){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta(ing1,ing2,ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza(mainIngredient, 
+    ...otherIngredients){
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    }
+
+};
+
+
+const rest = new Map();
+rest.set('name','Cafe de Rio');
+rest.set(1,'Firenze, Italy');
+console.log(rest.set(2,'Lisbon, Portugal'));
+
+rest.set('categories',['Italian', 'Pizzeria', 
+'Vegetarian', 'Organic'])
+.set('open',11)
+.set('close',23)
+.set(true,'We are open :D')
+.set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+
+//rest.clear();
+rest.delete(2);
+
+rest.set([1,2], 'Test');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get([1,2]));
+
+const arr = [1,2];
+rest.set(arr,'Test');
+console.log(rest.get(arr));
+rest.set(document.querySelector('h1'),'Heading');
+
+
+//// Maps : Iteration
+
+
+const question = new Map([
+  ['question','What is the best programming language in the world?'],
+  [1,'C'],
+  [2,'Java'],
+  [3,'JavaScript'],
+  ['correct',3],
+  [true,'Correct'],
+  [false,'Try Again!!!'],
+]);
+
+console.log(question);
+
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+// Quiz app
+
+for (const [key,value] of question ){
+  if(typeof key === 'number'){
+    console.log(`Answer ${key} : ${value}`);
+  }
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer ));
+
+console.log([...question]);
+
+//console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+*/
