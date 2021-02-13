fcb534458c6abfec7eda2178b1d1c90f702e6f01
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-13 15:56:55 +0600

    Update script.js

--- Data Structures , Modern Operators and Strings/script.js
+++ Data Structures , Modern Operators and Strings/script.js
@@ -713,3 +713,96 @@ for(const [key,{open,close}] of entries){
   console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
 */
+
+//////// SET
+/*
+const day =['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours = { 
+  [day[3]]: { // instead of thu:
+    open: 12,
+    close: 22,
+  },
+  [day[4]]: { // instead of fri:
+    open: 11,
+    close: 23,
+  },
+  [day[5]]: {  // instead of sat:
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery({
+    starterIndex = 1, 
+    mainIndex = 0,
+    time = '20:00', 
+    address,
+  }){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta(ing1,ing2,ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza(mainIngredient, 
+    ...otherIngredients){
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    }
+
+};
+
+const orderSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(orderSet);
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Biriyani'));
+orderSet.add('Garlic Bread');
+console.log(orderSet);
+orderSet.delete('Risotto');
+console.log(orderSet);
+//orderSet.clear();  // Delete all
+//console.log(orderSet); 
+
+
+//console.log(new Set('Marzan'));
+//console.log(new Set());
+//console.log(orderSet[0]); // in Set there is no index
+
+for(const order of orderSet){
+  console.log(order);
+}
+
+// Example
+
+const staff = ['Waiter','Chef','Waiter','Manager','Chef','Waiter'];
+
+const staffUnique = new Set(staff);
+console.log(staff);
+console.log(staffUnique);
+
+const staffUnique2 =[...new Set(staff)];
+console.log(staffUnique2);
+
+console.log(new Set(['Waiter','Chef','Waiter','Manager','Chef','Waiter']).size);
+
+console.log(new Set('Marzan Khan').size);
+*/
