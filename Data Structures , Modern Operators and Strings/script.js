d28968336e3155f92160f77d91cdf9621298d793
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-02-14 21:46:33 +0600

    Update script.js

--- Data Structures , Modern Operators and Strings/script.js
+++ Data Structures , Modern Operators and Strings/script.js
@@ -934,3 +934,213 @@ console.log([...question]);
 console.log(...question.keys());
 console.log(...question.values());
 */
+
+
+
+/*
+/////// String part 1
+
+const day =['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours = { 
+  [day[3]]: { // instead of thu:
+    open: 12,
+    close: 22,
+  },
+  [day[4]]: { // instead of fri:
+    open: 11,
+    close: 23,
+  },
+  [day[5]]: {  // instead of sat:
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];   
+  },
+
+  orderDelivery({
+    starterIndex = 1, 
+    mainIndex = 0,
+    time = '20:00', 
+    address,
+  }){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta(ing1,ing2,ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza(mainIngredient, 
+    ...otherIngredients){
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    }
+
+};
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('8737'[0]);
+
+console.log(airline.length);
+console.log('8737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4,7)); // begin and end
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ')+1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1,-1));
+
+const checkMiddleSeat = function(seat){
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if(s === 'B' || s === 'E'){
+    console.log('You got the middle seat');
+  }else{
+    console.log('You got lucky');
+  }
+}
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Marzan'));
+console.log(typeof new String('Marzan'));
+
+console.log(typeof new String('Marzan').slice(1));
+
+/////// String part 2
+
+console.log(airline.toLocaleLowerCase());
+console.log(airline.toLocaleUpperCase());
+
+// Fix Capitalization in name
+
+const passenger = 'mArZan';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase()+
+passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+
+const email = 'hello@marjan.com';
+const loginEmail = 'Hello@marjan.com \n';
+
+const lowerEmail = loginEmail.toLocaleLowerCase();
+const trimmedEmail = lowerEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing 
+
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£' , '$').replace(',','.');
+console.log(priceUS);
+
+const announcement = 'All passengers come to Boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door','gate'));
+console.log(announcement.replaceAll('door','gate'));
+
+console.log(announcement.replace(/door/,'gate'));
+console.log(announcement.replace(/door/g,'gate'));
+
+// Booleans
+
+const plane1 = 'A320neo';
+console.log(plane1.includes('A320'));
+console.log(plane1.includes('Boeing'));
+console.log(plane1.startsWith('Air')); // first 3 letters should be same
+
+if(plane1.startsWith('A32') && plane1.endsWith('neo'));{
+  console.log('Part of the new family');
+}
+
+// Practice
+
+const checkBaggage = function(items){
+  const baggage = items.toLocaleLowerCase();
+  if(baggage.includes('knife') || baggage.includes('gun')){
+    console.log('You are NOT allowed on board');
+  }else{
+    console.log('Welcome abroad');
+  }
+}
+
+checkBaggage('I have a laptop, some food and a pocket knife');
+checkBaggage('Socks and Camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+/////// String part 3
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Marzan Khan'.split(' '));
+
+const [firstName,lastName] = 'Jonas Khan'.split(' ');
+const newName = ['Mr.',firstName,lastName.toLocaleUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function(name){
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for(const n of names){
+    //namesUpper.push(n[0].toLocaleUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0],n[0].toLocaleUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+}
+capitalizeName('jessica and smith davis');
+capitalizeName('marzan khan');
+
+// Padding
+
+const message = 'Go to gate 23!';
+console.log(message.padStart(20,'+').padEnd(30,'+'));
+console.log('Marzan'.padStart(20,'+').padEnd(30,'+'));
+
+const maskCreditCard = function(number){
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length,'*');
+}
+console.log(maskCreditCard(43322344545454565565));
+console.log(maskCreditCard('34356556456665656'));
+
+// repeat
+
+const message2 = 'Bad weather...All Departures Delayed...';
+console.log(message2.repeat(3));
+
+const planesInline = function(n){
+  console.log(`There are ${n} planes in line ${':)'.repeat(n)}`);
+}
+planesInline(5);
+planesInline(3);
+planesInline(12);
+*/
