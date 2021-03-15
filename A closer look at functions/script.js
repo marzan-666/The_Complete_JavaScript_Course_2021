738644f58c201bfde8a51614738d9ab4e646d88c
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-15 23:48:14 +0600

    Update script.js

--- A closer look at functions/script.js
+++ A closer look at functions/script.js
@@ -333,3 +333,93 @@ poll.displayResults.call({answers: [5, 2, 3] },'string');
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1] },'string');
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1] });
 */
+
+/*
+// Immediately Invoked Function Expressions
+
+const runOnce = function(){
+    console.log('This will never run again. . .');
+    const isPrivate = 23;
+    var notPrivate = 46;
+}
+runOnce();
+//console.log(isPrivate);
+//console.log(notPrivate);
+
+// IIFE
+(function(){
+    console.log('This will never run again. . .');
+})();
+
+(()=>console.log('This will also never run again...'))();
+
+{
+    const isPrivate =23;
+    var notPrivate = 46;
+}
+//console.log(isPrivate);
+console.log(notPrivate);
+*/
+
+/*
+/// Closures
+
+const secureBooking = function(){
+    let passengerCount = 0;
+
+    return function(){
+        passengerCount++;
+        console.log(`${passengerCount} Passengers`);
+    };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+//console.dir(booker);
+
+// Examples of closures
+
+
+// Example 1
+
+let f;
+
+const g = function(){
+    const a = 23;
+    f = function(){
+        console.log(a*2);
+    };
+};
+
+const h = function() {
+    const b = 777;
+    f = function(){
+        console.log(b*2);
+    }
+}
+g();
+f();
+console.dir(f);
+
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function(n,wait){
+    const perGroup = n/3;
+
+    setTimeout(function(){
+        console.log(`We are now boarding all ${n} Passengers`);
+        console.log(`There are 3 Groups, each with ${perGroup} passengers`);
+    },wait*1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180,3);
+*/
