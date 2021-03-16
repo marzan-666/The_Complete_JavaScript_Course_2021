52d3722b751e3a0ce9193cfb293c80d9b827a31e
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-16 14:36:28 +0600

    Update script.js

--- Working with arrays/script.js
+++ Working with arrays/script.js
@@ -76,7 +76,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /*
 /////////////////////////////////////////////////
-// Simple Array Methods
+
+// ////// Simple Array Methods
+
 let arr = ['a','b','c','d','e'];
 console.log(arr.slice(2));
 console.log(arr.slice(2,4));
