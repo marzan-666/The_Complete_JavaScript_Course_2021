86593e87f5be3916f73a5ffb8fe25dc855cc9a8f
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-16 15:16:29 +0600

    Update script.js

--- Working with arrays/script.js
+++ Working with arrays/script.js
@@ -112,3 +112,44 @@ console.log([...arr,...arr2]);
 // JOIN
 console.log(letters.join('-'));
 */
+
+/*
+// Looping arrays forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+
+for(const movement of movements){
+  if(movement > 0){
+    console.log(`You deposited ${movement}`);
+  }else{
+    console.log(`You withdraw ${Math.abs(movement)}`);
+  }
+}
+for(const [i,movement] of movements.entries() ){
+  if(movement > 0){
+    console.log(`Movement ${i+1}: You deposited ${movement}`);
+  }else{
+    console.log(`Movement ${i+1}: You withdraw ${Math.abs(movement)}`);
+}
+}
+
+console.log('------FOR EACH------');
+movements.forEach(function(movement){
+  if(movement > 0){
+    console.log(`You deposited ${movement}`);
+  }else{
+    console.log(`You withdraw ${Math.abs(movement)}`);
+  }
+});
+console.log('------FOR EACH------');
+movements.forEach(function(mov,i,arr){
+  if(mov > 0){
+    console.log(`You deposited ${mov}`);
+  }else{
+    console.log(`You withdraw ${Math.abs(mov)}`);
+  }
+});
+//0: function(200)
+//1: function(450)
+//2: function(400)
+*/
