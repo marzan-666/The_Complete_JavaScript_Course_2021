dba1ae08d0f79c046b68ae91307f5af678fd529a
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-19 02:12:11 +0600

    Update script.js

--- Working with arrays/script.js
+++ Working with arrays/script.js
@@ -154,3 +154,87 @@ const checkDogs = function(dogsJulia,dogsKate){
 checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
 */
+
+/*
+/////////////
+/// The Map Method
+
+const eurToUsd = 1.1;
+const movementsUSD = movements.map(function(mov){
+  return mov * eurToUsd;
+});
+
+// const movementsUSD = movements.map(mov =>
+//   mov * eurToUsd);
+ // same thing with arrow
+
+
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for(const mov of movements) movementsUSDfor.push(mov*eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov,i) => 
+  `Movement ${i+1}: You ${mov > 0 ? 'deposited': 'withdrew'}${Math.abs(mov)}`
+//   {if(mov > 0){
+//     return`Movement ${i+1}:You deposited ${mov}`;
+//   }else{
+//     return `Movement ${i+1}:You withdraw ${Math.abs(mov)}`;
+//   }
+// }
+);
+
+console.log(movementsDescriptions);
+
+
+///////
+// The Filter Method
+
+const deposits = movements.filter(function(mov){
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for(const mov of movements) if (mov>0) depositsFor.push(mov);
+console.log(depositsFor);
+
+// const withdrawals = movements.filter(function(mov){
+//   return mov < 0;
+// });
+// console.log(withdrawals);
+
+const withdrawals = movements.filter(mov => mov <0);
+console.log(withdrawals);
+*/
+
+/*
+////////
+/// The Reduce method
+
+console.log(movements);
+
+const balance = movements.reduce(function(acc,cur,i,arr){
+  console.log(`Iteration ${i} : ${acc}`);
+  return acc + cur ;
+},0);
+console.log(balance);
+
+let balance2 = 0;
+for(const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc,mov)=>{
+  if(acc > mov){
+    return acc;
+  }else{
+    return mov;
+  }
+},movements[0]);
+console.log(max);
+*/
