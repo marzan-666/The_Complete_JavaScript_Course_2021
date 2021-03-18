cdeac1245ecb744402420ed25a0aacdd6b8efcd5
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-19 04:16:50 +0600

    Update script.js

--- Working with arrays/script.js
+++ Working with arrays/script.js
@@ -352,3 +352,17 @@ const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1,avg2);
 */
+
+/*
+//////
+//The Find method
+
+const firstWithdrawal = movements.find(mov => mov <0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+*/
