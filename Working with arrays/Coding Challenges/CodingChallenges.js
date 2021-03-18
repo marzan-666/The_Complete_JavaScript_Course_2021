a7fb875afbc783bb209168baa32e0821305aedff
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-18 23:00:37 +0600

    Update CodingChallenges.js

--- Working with arrays/Coding Challenges/CodingChallenges.js
+++ Working with arrays/Coding Challenges/CodingChallenges.js
@@ -18,3 +18,23 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+
+const checkDogs = function(dogsJulia,dogsKate){
+    const dogsJuliaCorrected = dogsJulia.slice();
+    dogsJuliaCorrected.splice(0,1);
+    dogsJuliaCorrected.splice(-2);
+    // dogsJuliaCorrected.slice(1,3);
+  
+    const dogs = dogsJuliaCorrected.concat(dogsKate);
+    console.log(dogs);
+  
+    dogs.forEach(function(dog,i){
+      if(dog >= 3){
+        console.log(`Dog number ${i+1} is an adult, and ${dog} years old`);
+      }else {
+        console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+      }
+    });
+  };
+  checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
+  checkDogs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
