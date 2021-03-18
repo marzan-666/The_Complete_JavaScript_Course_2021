ccbf82be799821dc0ab8d7f5f41ff3eaf6bf8434
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-03-19 03:43:54 +0600

    Update script.js

--- Working with arrays/script.js
+++ Working with arrays/script.js
@@ -276,3 +276,20 @@ const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1,avg2);
 */
+/*
+///////////////////////////////////////
+// The Magic of Chaining Methods
+
+const eurToUsd = 1.1;
+console.log(movements);
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
