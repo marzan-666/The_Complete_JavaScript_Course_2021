a9634bac3ff59d343b002eb67bcd4f556138b643
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-04-14 17:00:49 +0600

    Update script.js

--- Numbers, Dates, Intl and Timers/script.js
+++ Numbers, Dates, Intl and Timers/script.js
@@ -332,3 +332,30 @@ console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
 
 
+// The Remainder Operator
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+
+labelBalance.addEventListener('click',function(){
+  [...document.querySelectorAll('.movements__row')].forEach(function(row,i){
+    if(i % 2 === 0) row.style.backgroundColor = 'orangered';
+
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
