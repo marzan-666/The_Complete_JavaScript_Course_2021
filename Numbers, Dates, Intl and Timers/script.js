dee8916fabe1e7deaa5c3de589d5cc312d89c76a
Mohammad Marzan <38738228+marzan-666@users.noreply.github.com>
2021-04-14 16:43:57 +0600

    Update script.js

--- Numbers, Dates, Intl and Timers/script.js
+++ Numbers, Dates, Intl and Timers/script.js
@@ -284,3 +284,51 @@ console.log(Number.isInteger(20));
 console.log(Number.isInteger(20.0));
 console.log(Number.isInteger(20/0));
 
+
+/// Math and Rounding
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1/2));
+console.log(8 ** (1/3));
+
+console.log(Math.max(5,1,2,25,9));
+console.log(Math.max(5,1,2,'25',9));
+console.log(Math.max(5,1,2,'25px',9));
+
+console.log(Math.min(5,1,2,25,9));
+
+console.log(Math.PI);
+console.log(Math.PI * Number.parseFloat('10px')** 2);
+
+console.log(Math.trunc(Math.random()*6)+1);
+
+const randomInt = (min,max) => Math.trunc(Math.random()*(max-min)+1)+min;
+
+console.log(randomInt(10,20));
+
+
+// Rounding Integers
+
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+// Rounding decimals
+
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+
+
